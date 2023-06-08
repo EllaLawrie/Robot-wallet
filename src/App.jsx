2bade7a77854d2271047c6ebda347acf4b05ffc2
ekeoma1 <ekeigwee@gmail.com>
2023-06-08 22:25:16 +0100

    Eke's home page update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,16 @@
 // Components
-import Navbar from './components/navbar/Navbar'
-import Router from './components/Router'
+import Navbar from "./components/navbar/Navbar";
+import Router from "./components/Router";
 // import Modal from './components/modal/Modal'
 
 function App() {
   return (
-    <>
+    <div className="all-content-wrapper">
       <Navbar />
       {/* <Modal /> */}
       <Router />
-    </>
-  )
+    </div>
+  );
 }
 
-export default App
+export default App;
