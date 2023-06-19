b6232c64fd23c3b87690770d57b8ce1e2f9a0f59
EllaLawrie <lawrieella002@gmail.com>
2023-06-19 18:50:36 +0200

    added coinmarketapp

--- src/pages/home/HomePage.jsx
+++ src/pages/home/HomePage.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import { useState, useEffect } from 'react'
 // Components
 import Pricebox from '../../components/pricebox/Pricebox'
 import Action from '../../components/actions/Action'
